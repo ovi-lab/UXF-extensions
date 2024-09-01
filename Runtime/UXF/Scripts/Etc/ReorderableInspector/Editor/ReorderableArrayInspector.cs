d6d80bdd32f5a80ea64eb0dc82b66c9b69e71e65
Shariff Faleel <shariff.mfa@outlook.com>
2024-09-01 15:48:05 -0700

    Patch for bug with dalayCall

--- Runtime/UXF/Scripts/Etc/ReorderableInspector/Editor/ReorderableArrayInspector.cs
+++ Runtime/UXF/Scripts/Etc/ReorderableInspector/Editor/ReorderableArrayInspector.cs
@@ -60,9 +60,9 @@ namespace SubjectNerd.Utilities
 		[DidReloadScripts]
 		private static void HandleScriptReload()
 		{
-			FORCE_INIT = true;
+			FORCE_INIT = false;
 
-			EditorApplication.delayCall = () => { EditorApplication.delayCall = () => { FORCE_INIT = false; }; };
+			// EditorApplication.delayCall = () => { EditorApplication.delayCall = () => { FORCE_INIT = false; }; };
 		}
 
 		private static GUIStyle styleHighlight;
