10797eb763fb07374c44729fc8f13a2f1baab7e6
Shariff Faleel <shariff.mfa@outlook.com>
2025-08-18 16:17:58 -0700

    Correct worldToLocalMatrix headers and values
    Update PositionRotationTracker to correctly document and record the
    Transform.worldToLocalMatrix elements. Add XML seealso references and
    explicitly list the recorded fields (position, rotation quaternion,
    worldToLocalMatrix) in the class summary. Fix the CustomHeader names
    and the value tuples so that the matrix components use the correct
    indices: replace the incorrect wlm_04/14/24/34 with wlm_03/13/23/33
    and map them to m.m03, m.m13, m.m23, m.m33. This makes the CSV headers
    and their corresponding recorded values consistent with Unity's
    Matrix4x4 element naming and prevents mislabeling of matrix columns.

--- Runtime/UXF/Scripts/Trackers/PositionRotationTracker.cs
+++ Runtime/UXF/Scripts/Trackers/PositionRotationTracker.cs
@@ -8,15 +8,23 @@ namespace UXF
 {
     /// <summary>
     /// Attach this component to a gameobject and assign it in the trackedObjects field in an ExperimentSession to automatically record position/rotation of the object at each frame.
+    /// Records the:
+    /// - position (pos_x, pos_y, pos_z)
+    /// - rotation as quaternion (rot_x, rot_y, rot_z, rot_w)
+    /// - worldToLocalMatrix (wlm_00, wlm_01, wlm_02, wlm_03, wlm_10, wlm_11, wlm_12, wlm_13, wlm_20, wlm_21, wlm_22, wlm_23, wlm_30, wlm_31, wlm_32, wlm_33)
     /// </summary>
+    /// <seealso cref="Transform.position"/>
+    /// <seealso cref="Transform.rotation"/>
+    /// <seealso cref="Transform.worldToLocalMatrix"/>
+    /// <seealso cref="Matrix4x4"/>
     public class PositionRotationTracker : Tracker
     {
         public override string MeasurementDescriptor => "movement";
         public override IEnumerable<string> CustomHeader => new string[]
         {
             "pos_x", "pos_y", "pos_z", "rot_x", "rot_y", "rot_z", "rot_w",
-            "wlm_00", "wlm_01", "wlm_02", "wlm_04", "wlm_10", "wlm_11", "wlm_12", "wlm_14",
-            "wlm_20", "wlm_21", "wlm_22", "wlm_24", "wlm_30", "wlm_31", "wlm_32", "wlm_34",
+            "wlm_00", "wlm_01", "wlm_02", "wlm_03", "wlm_10", "wlm_11", "wlm_12", "wlm_13",
+            "wlm_20", "wlm_21", "wlm_22", "wlm_23", "wlm_30", "wlm_31", "wlm_32", "wlm_33",
         };
 
         /// <summary>
@@ -43,19 +51,19 @@ namespace UXF
                 ("wlm_00", m.m00),
                 ("wlm_01", m.m01),
                 ("wlm_02", m.m02),
-                ("wlm_04", m.m03),
+                ("wlm_03", m.m03),
                 ("wlm_10", m.m10),
                 ("wlm_11", m.m11),
                 ("wlm_12", m.m12),
-                ("wlm_14", m.m13),
+                ("wlm_13", m.m13),
                 ("wlm_20", m.m20),
                 ("wlm_21", m.m21),
                 ("wlm_22", m.m22),
-                ("wlm_24", m.m23),
+                ("wlm_23", m.m23),
                 ("wlm_30", m.m30),
                 ("wlm_31", m.m31),
                 ("wlm_32", m.m32),
-                ("wlm_34", m.m33)
+                ("wlm_33", m.m33)
             };
 
             return values;
